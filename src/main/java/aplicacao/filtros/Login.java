42beeb6dcbaf38319525d1bbd51623e1a031b909
Elcio <Elcio@DESKTOP-DV8CEDS>
2016-04-11 10:12:46 -0300

    ok

---
+++ src/main/java/aplicacao/filtros/Login.java
@@ -0,0 +1,23 @@
+package aplicacao.filtros;
+
+public class Login {
+	
+	private String ra;
+	private String senha;
+	
+	public String getRa() {
+		return ra;
+	}
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+
+}
