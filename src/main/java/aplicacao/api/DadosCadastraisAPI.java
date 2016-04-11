d961d236d536bd38268eaf81c45612cdb0a358c1
elciorodrigo <elciorodrigo@gmail.com>
2016-04-11 17:44:34 -0300

    Update DadosCadastraisAPI.java

--- src/main/java/aplicacao/api/DadosCadastraisAPI.java
+++ src/main/java/aplicacao/api/DadosCadastraisAPI.java
@@ -4,10 +4,9 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import aplicacao.controllers.DadosCadastraisController;
@@ -21,9 +20,6 @@ public class DadosCadastraisAPI {
 		private DadosCadastraisController dadosCadastrais;
 		
 		
-		
-		
-		
 		@CrossOrigin(origins = "*")
 		@RequestMapping(value = "/dadosCadastrais", method = RequestMethod.POST)
 		public DadosCadastrais listar( @RequestBody Login login) throws IOException {
@@ -33,13 +29,8 @@ public class DadosCadastraisAPI {
 			return dadosCadastrais.carregarDados(login);
 	    }
 	    
-	    
-	   
-
-
-	    
-	    
 		
 	
 	
 }
+
